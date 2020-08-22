f00c4550e7d20f6e191f0a9a67a1e1589088f262
rexholmstrom <rexholmstrom96@gmail.com>
2020-08-22 21:24:33 +0200

    keypress fixed

--- snake.js
+++ snake.js
@@ -11,6 +11,7 @@ let snake = [
   { x: 160, y: 200 },
 ];
 
+let changing_direction = false;
 let dx = 10;
 let dy = 0;
 
@@ -19,7 +20,12 @@ const snakeboard = document.getElementById("snakeboard");
 const snakeboard_ctx = snakeboard.getContext("2d");
 main();
 
+document.addEventListener("keydown", change_direction);
+
 function main() {
+  if (has_game_ended()) return;
+
+  changing_direction = false;
   setTimeout(function onTick() {
     clear_board();
     move_snake();
@@ -52,41 +58,52 @@ function drawSnakePart(snakePart) {
   snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
+function has_game_ended() {
+  for (let i = 4; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
+  }
+  const hitLeftWall = snake[0].x < 0;
+  const hitRightWall = snake[0].x > snakeboard.width - 10;
+  const hitToptWall = snake[0].y < 0;
+  const hitBottomWall = snake[0].y > snakeboard.height - 10;
+  return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+}
+
+function change_direction(event) {
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+
+  // Prevent the snake from reversing
+
+  if (changing_direction) return;
+  changing_direction = true;
+  const keyPressed = event.keyCode;
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+}
+
 function move_snake() {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(head);
   snake.pop();
-}
-
-function change_direction(event) {
-
-    const LEFT_KEY = 37;
-    const RIGHT_KEY = 39,
-    const DOWN_KEY = 38,
-    const UP_KEY = 40,
-
-
-    const keyPressed = event.keyCode;
-    const goingUP = dy === -10;
-    const goingDOWN = dy === 10;
-    const goingRIGHT = dx === 10;
-    const goingLEFT = dx === -10;
-
-    if (keyPressed === LEFT_KEY && !goingRIGHT ) {
-        dx = -10;
-        dy = 0;
-    } 
-    if (keyPressed === RIGHT_KEY && !goingLEFT ) {
-        dx = 10;
-        dy = 0;
-    } 
-    if (keyPressed === UP_KEY && !goingDOWN ) {
-        dx = 0;
-        dy = -10;
-    } 
-    if (keyPressed === DOWN_KEY && !goingUP ) {
-        dx = 0;
-        dy = 10;
-    } 
-
 }
