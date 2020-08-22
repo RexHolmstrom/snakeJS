1c7edb172d92dfc9fdf8e96e00ce3e2aa35fc380
rexholmstrom <rexholmstrom96@gmail.com>
2020-08-22 17:46:12 +0200

    cleanup

--- snake.js
+++ snake.js
@@ -13,12 +13,10 @@ let snake = [
 
 // Get the canvas element
 const snakeboard = document.getElementById("snakeboard");
-// Return a two dimensional drawing context
 const snakeboard_ctx = snakeboard.getContext("2d");
-// Start game
-main();
 
-// main function called repeatedly to keep the game running
+
+main();
 function main() {
     clearCanvas();
     drawSnake();
@@ -30,28 +28,20 @@ function clearCanvas() {
   snakeboard_ctx.fillStyle = board_background;
   //  Select the colour for the border of the canvas
   snakeboard_ctx.strokestyle = board_border;
-  // Draw a "filled" rectangle to cover the entire canvas
   snakeboard_ctx.fillRect(0, 0, snakeboard.width, snakeboard.height);
-  // Draw a "border" around the entire canvas
   snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
 }
 
 // Draw the snake on the canvas
 function drawSnake() {
-  // Draw each part
   snake.forEach(drawSnakePart)
 }
 
 // Draw one snake part
 function drawSnakePart(snakePart) {
 
-  // Set the colour of the snake part
   snakeboard_ctx.fillStyle = snake_col;
-  // Set the border colour of the snake part
   snakeboard_ctx.strokestyle = snake_border;
-  // Draw a "filled" rectangle to represent the snake part at the coordinates
-  // the part is located
   snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-  // Draw a border around the snake part
   snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
